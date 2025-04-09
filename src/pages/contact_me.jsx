784f6f8badc997e04bc6a112eb462f6e978db901
alejo <alejoreyvm@hotmail.com>
2025-04-09 11:16:18 -0500

    segundo commit

--- src/pages/contact_me.jsx
+++ src/pages/contact_me.jsx
@@ -1,10 +1,12 @@
+import Yo from '../assets/personal.jpeg'
+
 export function Contact(){
 
     return(
 
         <div className="contact" id="contact">
 
-        <img src="https://static-00.iconduck.com/assets.00/user-icon-1024x1024-unb6q333.png" className="size-100" alt="" />
+        <img src={Yo} className="personal" alt="" />
         
         <div className="contactCard opacity-0 animate-pop-in">
 
@@ -14,7 +16,7 @@ export function Contact(){
             <span> <i class="bi bi-envelope-at-fill"></i>  Alejoreyvm@hotmail.com</span>
             <span> <i class="bi bi-telephone-fill"></i>  314 282 2521</span>
             <span> <a href="https://github.com/alejocoding"> <i class="bi bi-github"></i> GitHub AlejoCoding</a></span>
-            <span>  <i class="bi bi-building"></i> Ibague-Tolima</span>
+            <span> <i class="bi bi-building"></i> Ibague-Tolima</span>
         </div>
 
         
